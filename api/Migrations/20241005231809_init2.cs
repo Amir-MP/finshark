d99a37d16b8f942b252f93b4d6f416268ace11b8
amir mp <amirmpr@outlook.com>
2024-10-06 02:55:55 +0330

    up

---
+++ api/Migrations/20241005231809_init2.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Users_StockId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Stocks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Stocks",
+                table: "Stocks",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments",
+                column: "StockId",
+                principalTable: "Stocks",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Stocks",
+                table: "Stocks");
+
+            migrationBuilder.RenameTable(
+                name: "Stocks",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Users_StockId",
+                table: "Comments",
+                column: "StockId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
